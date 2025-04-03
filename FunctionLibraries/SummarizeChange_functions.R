0e2642df201a52166880c0c7b06e225636bbe8d5
Lauren Cox <lecox828@gmail.com>
2025-04-03 15:16:23 -0500

    Added "zonal.calculations.single.raster.exact" function to test partial pixel calculation time.

--- FunctionLibraries/SummarizeChange_functions.R
+++ FunctionLibraries/SummarizeChange_functions.R
@@ -289,6 +289,15 @@ zonal.calculations.single.raster<-function(rster,mapun,prepVec){
   return(zonal.stats.summarypoly)
 }
 
+zonal.calculations.single.raster.exact<-function(rster,mapun,prepVec){
+  summaryzonal.time<- system.time(zonal.stats.summarypoly<-zonal(rster,prepVec$sumPoly,fun="mean",as.polygons=TRUE,na.rm=TRUE,exact=TRUE) )
+  print(paste("Zonal stats calculated for ",names(rster), " using ", mapun, sep=""))
+  print(summaryzonal.time/60)
+  
+  return(zonal.stats.summarypoly)
+}
+
+
 
 #***TODO edit this one to take one raster of the veg classification, and a list of rasters to be masked
 #this function assumes we want a 'current' vegetation classification
