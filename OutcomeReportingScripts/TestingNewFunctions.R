311a661418a312e68ad3e9c970184c96555e866a
MV Eitzel <mveitzel@gmail.com>
2025-07-19 09:44:29 -0700

    Adding a bit more to the testing new functions script

--- OutcomeReportingScripts/TestingNewFunctions.R
+++ OutcomeReportingScripts/TestingNewFunctions.R
@@ -1,2 +1,14 @@
 #TestingNewFunctions.R
 
+source("SummarizeChange_functions.R")
+
+#setwd("D:/DropboxFiles/Dropbox/Professional/UCB_Battles/TreatmentEffectiveness")
+setwd("/home/mves/Dropbox/Professional/UCB_Battles/TreatmentEffectiveness")
+
+boundary.shape<-c("Region_Sierra.shp")
+boundary.name<-c("Sierra")
+
+
+
+
+read.and.prepare.boundary.vector
